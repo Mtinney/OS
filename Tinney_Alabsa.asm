037cff09b4231488187392259ecef200f587488c
Shade Alabsa <sahde34321@gmail.com>
2014-06-01 14:26:03 -0400

    Cleaned up the code so comments were on the same line

--- Tinney_Alabsa.asm
+++ Tinney_Alabsa.asm
@@ -1,31 +1,39 @@
+
 BITS 16
 start:
-   mov ax, 07C0h
-   add ax, 288
+   mov ax, 07C0h ; Set up 4K stack space after this bootloader
+   add ax, 288 ;  (4096 + 512) / 16 bytes per paragraph
    mov ss, ax
    mov sp, 4096
-   mov ax, 07C0h
+
+   mov ax, 07C0h  ; Set data segment to where we're loaded
    mov ds, ax
-mov si, boot_message call print_string
-; Set up 4K stack space after this bootloader ; (4096 + 512) / 16 bytes per paragraph
-; Set data segment to where we're loaded
-; Put message string position into SI ; Call printing routine
-jmp $
+
+
+mov si, boot_message   ; Put message string position into SI
+call print_string      ; Call printing routine
+
+jmp $    ; Jump here - infinite loop!
+
 boot_message db 'Booting from floppy...', 0
 cs_message db 'CS-DOS Operating System v1.0', 0
 creating_message db 'Created for CS3423, Summer 2014', 0
 copyright db '(C) Copyright 2014 by Matthew Tinney and Shade Alabsa. All rights reserved.', 0
-; Jump here - infinite loop!
-print_string: mov ah, 0Eh
+
+print_string:   ; Routine: output string in SI to screen
+    mov ah, 0Eh  ; 'print char' function is denoted by 0Eh
+
 .repeat:
-   lodsb
-   cmp al, 0
-   je .done
-   int 10h
+   lodsb    ; load next byte from SI and increment SI. 
+   cmp al, 0s  ; compare to zero to see if end of string. 
+   je .done ; if char is zero, we’re at end of string. 
+   int 10h  ; Otherwise, print it.
    jmp .repeat
-.done: ret
-times 510-($-$$) db 0 dw 0xAA55
-; Routine: output string in SI to screen ; 'print char' function is denoted by 0Eh
-; load next byte from SI and increment SI. ; compare to zero to see if end of string. ; if char is zero, we’re at end of string. ; Otherwise, print it.
-; fill remainder of boot sector with 0s up ; to byte 510.
-; The standard PC boot signature of AA 55 ; is placed in bytes 511 and 512.
+.done: 
+   ret
+
+times 510-($-$$) db 0 ; fill remainder of boot sector with 0s up ; to byte 510.
+dw 0xAA55   ; The standard PC boot signature of AA 55 ; is placed in bytes 511 and 512.
+ 
+
+
